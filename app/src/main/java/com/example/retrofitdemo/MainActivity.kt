ac590a45cc469b3ff63c4edebd09faa66bf932f9
沉璧浮光 <120276969@qq.com>
2021-01-05 10:55:26 +0800

    代码调整，移除测试代码

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.example.retrofitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
-import androidx.viewbinding.ViewBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.example.retrofitdemo.databinding.ActivityMainBinding
 import com.example.retrofitdemo.tools.RequestStatus
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.HttpException
-import retrofit2.create
-import retrofit2.http.GET
-import java.net.SocketException
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
-import javax.net.ssl.SSLException
 
 class MainActivity : AppCompatActivity() {
-
     // 利用系统扩展的代理，快速生成viewModel
     private val viewModel by viewModels<MainViewModel>()
 
@@ -38,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         viewBinding.recyclerView.adapter = mAdapter
 
         // 定义LiveData
-        viewModel.newsLiveData.observe(this, Observer {
+        viewModel.newsLiveData.observe(this, {
             when (it.requestStatus) {
                 RequestStatus.START -> {
                 }
@@ -48,16 +33,14 @@ class MainActivity : AppCompatActivity() {
                         mAdapter.setList(newsBean.result)
                     }
                 }
-                RequestStatus.COMPLETE -> {
-                    Toast.makeText(this, "网络请求完成了", Toast.LENGTH_SHORT).show()
-                }
                 RequestStatus.ERROR -> {
                     Toast.makeText(this, "网络出错了", Toast.LENGTH_SHORT).show()
                 }
+                RequestStatus.COMPLETE -> {
+                    Toast.makeText(this, "网络请求完成了", Toast.LENGTH_SHORT).show()
+                }
             }
         })
-
-
     }
 
     override fun onStart() {
